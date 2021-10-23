d7402852bb0939d223a37aa16b80e4eccb62bbc5
xiaozou <1424113865@qq.com>
2021-10-23 23:06:44 +0800

    demo2

--- demo1/main.js
+++ demo1/main.js
@@ -5,7 +5,7 @@ let road = document.getElementById("road");
 let text = document.getElementById("text");
 
 window.addEventListener("scroll", function () {
-    var value = window.scrollY;
+    let value = window.scrollY;
 
     bg.style.top = value * 0.5 + "px";
     moon.style.left = -value * 0.5 + "px";
